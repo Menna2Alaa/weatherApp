e3f3ad19f2f09c008ddcfc4f1b4b562dcf69e12d
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-06-27 23:49:52 +0300

    change the color of the app bar in search view according to weather state

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:weather_app/main.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
-          'Search a city',
-          style: TextStyle(color: Colors.white),
-        ),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            onSubmitted: (value) async {
-              var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
-              getWeatherCubit.getWeather(cityName: value);
-              Navigator.pop(context);
-            },
-            decoration: InputDecoration(
-              labelText: ('Search'),
-              hintText: 'Enter city name',
-              suffixIcon: Icon(Icons.search),
-              contentPadding: EdgeInsets.symmetric(
-                vertical: 32,
-                horizontal: 16,
+    return BlocBuilder<GetWeatherCubit, WeatherState>(
+      builder: (context, state) {
+        String? weatherCondition;
+        if (state is WeatherDataLoaded) {
+          weatherCondition = state.weatherModel.weatherCondition;
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: getThemeColor(weatherCondition),
+            title: const Text(
+              'Search a city',
+              style: TextStyle(color: Colors.white),
+            ),
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                onSubmitted: (value) async {
+                  var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(
+                    context,
+                  );
+                  getWeatherCubit.getWeather(cityName: value);
+                  Navigator.pop(context);
+                },
+                decoration: InputDecoration(
+                  labelText: ('Search'),
+                  hintText: 'Enter city name',
+                  suffixIcon: Icon(Icons.search),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 32,
+                    horizontal: 16,
+                  ),
+                  // border: OutlineInputBorder(
+                  //   borderSide: BorderSide(color: Colors.orange),
+                  // ),
+                ),
               ),
-              // border: OutlineInputBorder(
-              //   borderSide: BorderSide(color: Colors.orange),
-              // ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
